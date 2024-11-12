2f0969246b076c56f28a22d28f8ce1071c01bfb4
Mosehchan <mosesnthenge06@gmail.com>
2024-11-12 10:51:21 +0300

    Add files via upload

---
+++ 1D arrays.c
@@ -0,0 +1,15 @@
+//arrays in c - 1d arrays
+/*
+author: moses
+reg no:CT101/G/22682/24
+date:19/10/2024
+*/
+#include<stdio.h>
+int main(){
+	int k;
+	int plants[6] = {34,67,56,19,50,79};
+	for(k=0;k<6;k++){
+		printf("plants[%d] = %d \n",k, plants[k]);
+	}
+	return 0;
+}
